49642fdf6c9bed3e6b981901b205d465fb05d3a8
jieun <kad1370@naver.com>
2019-07-18 22:34:43 +0900

    3. 불필요한 mvc 및 java / jsp 폴더 삭제
    <세부사항>
    삭제업무: area업무폴더 (java/SQL/jsp)
    - area업무폴더는 지역 데이터를 화면에 메뉴로 그려주는 업무였으나
    해당 기능이 필요가 없어졌기에 삭제

--- goTravel/src/main/java/com/project/goTravel/main/web/MainController.java
+++ goTravel/src/main/java/com/project/goTravel/main/web/MainController.java
@@ -1,15 +1,10 @@
 package com.project.goTravel.main.web;
 
-import java.util.List;
-import java.util.Map;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import com.project.goTravel.main.service.MainService;
 
@@ -20,12 +15,8 @@ public class MainController {
 	private MainService mainService;
 	
 	@RequestMapping("/") 
-	public String initMain(HttpServletRequest req, @RequestParam Map<String, Object> map, ModelMap model) throws Exception {
+	public String initMain() throws Exception {
 		
-		List<Map<String,Object>> suksoTypeList = mainService.selectMainServiceList();
-		System.out.println("메뉴리스트확인: " + suksoTypeList);
-		
-		model.addAttribute("suksoTypeList", suksoTypeList);
 		return "main/main.tiles";
 	}
 }
